9e8bcc2887e366ff9ef31cc8ed2be24174d826ac
Eduardo Freitas de Carvalho <eduardofc@ic.ufrj.br>
2026-04-21 18:26:58 -0300

    chore(database): fix engine usage that were breaking integration tests

--- challenge-01-acessibilidade/src/infra/database.py
+++ challenge-01-acessibilidade/src/infra/database.py
@@ -1,28 +1,45 @@
 """Database configuration and setup."""
 
 import os
+from typing import Optional
 
+from sqlalchemy.engine import Engine
 from sqlmodel import Session, SQLModel, create_engine
 
-# Load environment variables from .env file
 DATABASE_URL = os.getenv("DATABASE_URL")
 ECHO_ENABLED = os.getenv("ECHO_ENABLED", "true") == "true"
 
-# Create engine
-engine = create_engine(
-    DATABASE_URL,
-    echo=ECHO_ENABLED,
-    connect_args={"check_same_thread": False},
-)
+_engine: Optional[Engine] = None
+
+
+def _get_engine() -> Engine:
+    global _engine
+
+    if _engine is None:
+        _engine = create_engine(
+            DATABASE_URL,
+            echo=ECHO_ENABLED,
+            connect_args={"check_same_thread": False},
+        )
+
+    return _engine
+
+
+def set_engine(engine: Engine):
+    global _engine
+    _engine = engine
 
 
 def create_db_and_tables():
     """Create database and tables."""
-    SQLModel.metadata.create_all(engine)
+    SQLModel.metadata.create_all(_get_engine())
 
 
-# Generator that yeilds
 def get_session():
     """Get database session."""
-    with Session(engine) as session:
+    with Session(_get_engine()) as session:
         yield session
+
+
+def engine() -> Engine:
+    return _get_engine()
