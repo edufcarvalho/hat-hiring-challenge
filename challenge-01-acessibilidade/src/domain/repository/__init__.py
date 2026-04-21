a1fc65aeff3a3353f83773569017c6f69fc1bebf
Eduardo Freitas de Carvalho <eduardofc@ic.ufrj.br>
2026-04-20 23:42:15 -0300

    fix: typo and pyproject redundacy

--- challenge-01-acessibilidade/src/domain/repository/__init__.py
+++ challenge-01-acessibilidade/src/domain/repository/__init__.py
@@ -1,4 +1,4 @@
-from src.domain.repository.gasto_reposity import GastoRepository
+from src.domain.repository.gasto_repository import GastoRepository
 from src.domain.repository.orgao_repository import OrgaoRepository
 
 __all__ = ["GastoRepository", "OrgaoRepository"]
