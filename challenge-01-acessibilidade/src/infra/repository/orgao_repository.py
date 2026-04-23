aa9059a06eba1b8cc5d98a6d60c38506590ade8b
Eduardo de Carvalho <eduardofc@ic.ufrj.br>
2026-04-23 09:07:09 -0300

    fix(repository): add orgao filtering to OrgaoRepository

--- challenge-01-acessibilidade/src/infra/repository/orgao_repository.py
+++ challenge-01-acessibilidade/src/infra/repository/orgao_repository.py
@@ -2,8 +2,14 @@ from sqlmodel import Session
 
 from src.domain.models import Orgao
 from src.infra.repository.base_repository import BaseRepository
-
+from src.domain.schemas import OrgaoParams as Params
 
 class OrgaoRepository(BaseRepository):
     def __init__(self, session: Session):
         super().__init__(session, Orgao)
+
+    def _apply_filters(self, query, params: Params):
+        if params.orgao:
+            return query.where(self.model.nome == params.orgao)
+
+        return query
---
+++ challenge-01-acessibilidade/src/tests/test_orgao_repository.py
@@ -0,0 +1,34 @@
+from src.domain.models import Orgao
+from src.domain.schemas import OrgaoParams as Params, PaginatedResponse
+from src.tests.utils import BaseTest
+from src.infra.repository import OrgaoRepository
+
+class TestOrgaoRepository(BaseTest):
+    def setUp(self):
+        super().setUp(Orgao)
+        self.repository = OrgaoRepository(self.session) 
+
+    def test_list_all(self):
+        """Test .list_all without passing any filtering params"""
+        params = Params()
+        result = self.repository.list_all(params)
+
+        self.assertTrue(all(isinstance(item, Orgao) for item in result.items), "All returns should be Orgaos")
+        self.assertIsInstance(result, PaginatedResponse, "Response should be paginated")
+
+    def test_list_all_filtering(self):
+        """Test .list_all filtering looking for a specific Orgao"""
+        params = Params(orgao="Ministerio B")
+        result = self.repository.list_all(params)
+
+        self.assertEqual(result.items[0].nome, "Ministerio B", "Should return the filtered Orgao")
+        self.assertIsInstance(result.items[0], Orgao, "Should return an Orgao")
+        self.assertIsInstance(result, PaginatedResponse, "Response should be paginated")
+
+    def test_list_all_filtering_for_an_inexistant_orgao(self):
+        """Test .list_all filtering looking for a inexistant Orgao"""
+        params = Params(orgao="Ministerio da Magia")
+        result = self.repository.list_all(params)
+
+        self.assertIsInstance(result, PaginatedResponse, "Response should be paginated")
+        self.assertEqual(len(result.items), 0, "Should return an empty list")
