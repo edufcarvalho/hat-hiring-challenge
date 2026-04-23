a41b8f6cfec1ef906e664e7937d9443f79be665c
Eduardo Freitas de Carvalho <eduardofc@ic.ufrj.br>
2026-04-22 21:31:44 -0300

    fix(validation): add validation to ensure value_min less or equal than value_max

--- challenge-01-acessibilidade/src/domain/schemas.py
+++ challenge-01-acessibilidade/src/domain/schemas.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator
 
 from src.domain.models import Gasto
 
@@ -25,3 +25,11 @@ class Params(BaseModel):
     categoria: Optional[str] = None
     valor_min: Optional[Decimal] = None
     valor_max: Optional[Decimal] = None
+
+    @model_validator(mode="after")
+    def min_should_be_le_than_max(self):
+        if self.valor_min is not None and self.valor_max is not None:
+            if self.valor_min >= self.valor_max:
+                raise ValueError("valor_min cannot be greater than valor_max")
+
+        return self
